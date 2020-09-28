b6366a2d401b50492a58bbac1852e7165bf13a04
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-09-28 16:04:26 +0200

    Show forecast data in UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,23 @@
 const weatherForm = document.querySelector('form');
 const searchInput = document.querySelector('input');
+const messageOne = document.getElementById('message-1');
+const messageTwo = document.getElementById('message-2');
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const location = searchInput.value;
 
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
   fetch('http://localhost:3000/weather?address=' + location).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
-          console.log(data.error);
+          messageOne.textContent = data.error;
         } else {
-          console.log(data.location);
-          console.log(data.forecast);
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
         }
       });
     }
