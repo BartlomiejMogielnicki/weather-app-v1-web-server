e32d93c825bff866497a5544a7926d0db4df3e13
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-09-28 12:21:06 +0200

    Render dynamic content with handlebars (hbs)

--- src/app.js
+++ src/app.js
@@ -4,7 +4,29 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'John Smith',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'John Smith',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    message: 'This is a test message on the help page.',
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
